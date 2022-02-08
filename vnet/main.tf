2433df0da4573d6494e25d128d2a9eac49cf8a12
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-08 16:11:20 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -24,7 +24,8 @@ module "azurerm_virtual_network" {
     source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-vnet?ref=main"
     name                      = var.virtual_network_name
     resource_group_name       = azurerm_resource_group.rg.name
-    location                  = azurerm_resource_group.rg.location    
+    location                  = azurerm_resource_group.rg.location 
+    dns_servers               = "azure DNS"
     address_space             = ["172.16.0.0/16"]
 }
 module "azurerm_subnet" {
