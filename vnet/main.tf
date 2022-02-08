9d4fe1a1171dcf9967b064554a996640dd750eae
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-08 13:40:52 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -20,94 +20,22 @@ resource "azurerm_network_security_rule" "nsgrules" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
-module "azurerm_virtual_network" "vnet" {
+module "azurerm_virtual_network" {
+    source = "git::https://github.com/Priyasanampudi/terraform_resources/modules.git//tf-module-vnet?ref=dev"
     name                      = var.virtual_network_name
     resource_group_name       = azurerm_resource_group.rg.name
     location                  = azurerm_resource_group.rg.location    
     address_space             = ["172.16.0.0/16"]
 }
 module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name1"
-    address_prefix = "172.16.1.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name2"
-    address_prefix = "172.16.2.0/24"
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name3"
-    address_prefix = "172.16.3.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name4"
-    address_prefix = "172.16.4.0/24"
+    source = "git::https://github.com/Priyasanampudi/terraform_resources/tree/dev/modules/tf-module-subnet"
+    depends_on           = [module.azurerm_virtual_network]
+    name           = "var.subnet_name"
+    address_prefix = "var.subnet_address_space"
     security_group = azurerm_network_security_group.nsg.id
 }
 
 tags = {
     createdBy = "Priyanka"
     application = "DigitalIntegrationHubAccelerator"
-=======
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-resource "azurerm_network_security_group" "nsg" {
-    name                      = var.network_security_group_name
-    resource_group_name       = azurerm_resource_group.rg.name
-    location                  = azurerm_resource_group.rg.location
-}
-resource "azurerm_network_security_rule" "nsgrules" {
-  name                        = var.network_security_rule_name
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-module "azurerm_virtual_network" "vnet" {
-    name                      = var.virtual_network_name
-    resource_group_name       = azurerm_resource_group.rg.name
-    location                  = azurerm_resource_group.rg.location    
-    address_space             = ["172.16.0.0/16"]
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name1"
-    address_prefix = "172.16.1.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name2"
-    address_prefix = "172.16.2.0/24"
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name3"
-    address_prefix = "172.16.3.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-}
-module "azurerm_subnet" {
-    source = ""
-    name           = "var.subnet_name4"
-    address_prefix = "172.16.4.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-}
-
-tags = {
-    createdBy = "Priyanka"
-    application = "DigitalIntegrationHubAccelerator"
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
 }
