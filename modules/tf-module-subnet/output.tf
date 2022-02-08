511411e62e034453b026a7da51207eb9fcd3ec12
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-08 13:53:52 +0530

    Update output.tf

--- modules/tf-module-subnet/output.tf
+++ modules/tf-module-subnet/output.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "id" {
     value = azurerm_subnet.subnet.id
 }
@@ -14,20 +13,3 @@ output "virtual_network_name" {
 output "address_prefixes" {
     value = azurerm_subnet.subnet.address_prefixes
 }
-=======
-output "id" {
-    value = azurerm_subnet.subnet.id
-}
-output "name" {
-    value = azurerm_subnet.subnet.name
-}
-output "resource_group_name" {
-    value = azurerm_subnet.subnet.resource_group_name
-}
-output "virtual_network_name" {
-    value = azurerm_subnet.subnet.virtual_network_name
-}
-output "address_prefixes" {
-    value = azurerm_subnet.subnet.address_prefixes
-}
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
