e34b429b29686b7010132a3d8abc80047adc0cc5
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-08 16:06:34 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -29,9 +29,9 @@ module "azurerm_virtual_network" {
 }
 module "azurerm_subnet" {
     source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-vnet?ref=main"
-    vnet_name            = "var.virtual_network_name"
+    name                 = var.azurerm_subnet
+    vnet_name            = var.virtual_network_name
     resource_group_name  = azurerm_resource_group.rg.name
-    azurerm_subnet       = "var.azurerm_subnet"
-    address_prefixes     = "var.subnet_address_space"
+    address_prefixes     = var.subnet_address_space
     tags                 = var.tags
 }
