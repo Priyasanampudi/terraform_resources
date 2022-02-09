c6488b14635143a24fedd6b0feb0ef2a0af9b189
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 15:18:30 +0530

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,7 +1,7 @@
 module "apimsubnet" {
     source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-subnet?ref=main"
     subnet_name          = var.subnet_name
-    resource_group_name  = azurerm_resource_group.rg.name
+    resource_group_name  = var.resource_group_name
     vnet_name            = var.vnet_name 
     subnet_address_space = var.subnet_address_space
     enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint_network_policies
