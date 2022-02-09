9fab520e7f9457ac352d744039674d08f5cc4b8b
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 14:43:40 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_security_rule" "nsgrules" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "*"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
