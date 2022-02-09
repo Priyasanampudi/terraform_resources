9c445fa4aeb6321e13d1e5e0329986b3ee755348
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 13:33:41 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -22,11 +22,5 @@ resource "azurerm_network_security_rule" "nsgrules" {
 }
 module "vnet" {
     source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-vnet?ref=main"
-    name                      = "myvnet456"
-    resource_group_name       = azurerm_resource_group.rg.name
-    location                  = azurerm_resource_group.rg.location 
-    dns_servers               = "azure DNS"
-    address_space             = var.vnet_address_space
-    tags                      = var.tags
 }
 
