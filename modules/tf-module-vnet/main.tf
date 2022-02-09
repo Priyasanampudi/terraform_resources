4621de2838f39c24d4fd2f017156d5265475b2a4
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 14:10:59 +0530

    Update main.tf

--- modules/tf-module-vnet/main.tf
+++ modules/tf-module-vnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   address_space       = var.vnet_address_space
   tags                = var.tags
   
