3fa300c20bdd28f19ae32b0cc5c218b0b1861897
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 15:16:52 +0530

    Update variables.tf

--- subnet/variables.tf
+++ subnet/variables.tf
@@ -1,16 +1,21 @@
 variable "resource_group_name" {
-  description = "Name of the resource group"
-  type = string
+    description = "Name of the resource group"
+    type = string
 }
 variable "vnet_name" {
-  description = "Name of the virtual network"
-  type = string
+    description = "Name of the virtual network"
+    type = string
 }
 variable "subnet_name" {
     description = "Name of the subnet"
     type = string
 }
 variable "subnet_address_space" {
-    description = "adress prefix of subnet"
-    type = string
+    type = list(string)
+    description = "Subnet prefix address list"
+}
+variable "enforce_private_link_endpoint_network_policies" {
+    type = bool
+    description = "private_link_endpoint_network_policies"
+    default = false
 }
