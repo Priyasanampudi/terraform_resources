3d166bcfb9cb9a4e9d341b1086fab6208d34408f
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-09 14:06:09 +0530

    Update variable.tf

--- modules/tf-module-vnet/variable.tf
+++ modules/tf-module-vnet/variable.tf
@@ -7,7 +7,7 @@ variable "vnet_name"{
   description = "Provide vnet name"
 }
 variable "vnet_address_space" {
-  type        = list(string)
+  type        = string
   description = "The address space that is used by the virtual network."
   default     = ["10.0.0.0/16"]
 }
