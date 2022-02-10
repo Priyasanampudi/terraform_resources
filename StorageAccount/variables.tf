f2d97c5746f846868c07c91a5c15e952932df353
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 11:51:39 +0530

    Update variables.tf

--- StorageAccount/variables.tf
+++ StorageAccount/variables.tf
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 variable "location" {
   description = "Name of the location where the resources will be provisioned"
   type = string
 }
-
 variable "resource_group_name" {
   description = "Name of the resource group"
   type = string
 }
-
 variable "storage_account_name" {
   description = "Name of the storage account"
   type = string
@@ -20,27 +17,12 @@ variable "virtual_network_name" {
 variable "subnet_name" {
   description = "Name of the storage account"
   type = string
-=======
-variable "location" {
-  description = "Name of the location where the resources will be provisioned"
+}
+variable "account_tier" {
+  description = "tier"
   type = string
 }
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
+variable "account_replication_type" {
+  description = "replication"
   type = string
-}
-
-variable "storage_account_name" {
-  description = "Name of the storage account"
-  type = string
-}
-variable "virtual_network_name" {
-  description = "Name of the storage account"
-  type = string
-}
-variable "subnet_name" {
-  description = "Name of the storage account"
-  type = string
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
 }
