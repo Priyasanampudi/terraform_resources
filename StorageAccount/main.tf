f47b5226e1760db3db6a5696b4d9c595ae55ea68
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 13:15:55 +0530

    Update main.tf

--- StorageAccount/main.tf
+++ StorageAccount/main.tf
@@ -11,6 +11,7 @@ module "coresubnet" {
   resource_group_name  = var.resource_group_name
   subnet_name          = var.subnet_name
   subnet_address_space = var.subnet_address_space
+  service_endpoints    = ["Microsoft.Storage"]
 }
 module "storageAccount" {
   source                   = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-storage-account?ref=main"
@@ -29,6 +30,6 @@ resource "azurerm_storage_account_network_rules" "sa_rules" {
 
   default_action             = "Allow"
   ip_rules                   = ["127.0.0.1"]
-  virtual_network_subnet_ids = data.vnet.id
+  vnet_subnet_ids            = module.coresubnet.id
   bypass                     = ["Metrics"]
 }
