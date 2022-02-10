ef8771ed41d3d390e81f66feb28589a1d3410a5c
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 12:16:28 +0530

    Update output.tf

--- modules/tf-module-storage-account/output.tf
+++ modules/tf-module-storage-account/output.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "id" {
   value = azurerm_storage_account.stacnt.id
   description = "The storage account id"
@@ -12,18 +11,3 @@ output "primary_key" {
   value = azurerm_storage_account.stacnt.primary_access_key
   sensitive   = true
 }
-=======
-output "id" {
-  value = azurerm_storage_account.stacnt.id
-  description = "The storage account id"
-}
-output "name" {
-  value = azurerm_storage_account.stacnt.name
-  description = "The storage account name"
-}
-output "primary_key" {
-  description = "The primary access key for the storage account"
-  value = azurerm_storage_account.stacnt.primary_access_key
-  sensitive   = true
-}
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
