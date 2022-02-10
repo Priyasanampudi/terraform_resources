20781d290db3c673b3dabc59e0ea149ccf841314
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 16:31:22 +0530

    Update main.tf

--- appService/main.tf
+++ appService/main.tf
@@ -17,5 +17,7 @@ resource "azurerm_app_service" "appservice" {
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-  site_config         = var.site_config
+  site_config {
+    dotnet_framework_version = "v4.0"
+  }
 }
