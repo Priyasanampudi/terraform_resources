9edea087b07d82edab99071a731d47f2ab84c438
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 11:45:43 +0530

    Update providers.tf

--- StorageAccount/providers.tf
+++ StorageAccount/providers.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
     azurerm = {
@@ -17,24 +16,4 @@ terraform {
     container_name       = "tfstatefile"
     key                  = "sa.tfstate"
   }
-=======
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.91"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-}
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "test-githubactions"
-    storage_account_name = "tfstatesa29"
-    container_name       = "tfstatefile"
-    key                  = "sa.tfstate"
-  }
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
 }
