34535a241c0c8b160d35e9f7bbd95a208220f6de
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 17:36:31 +0530

    Update main.tf

--- modules/tf-module-subnet/main.tf
+++ modules/tf-module-subnet/main.tf
@@ -13,6 +13,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name                             = data.azurerm_resource_group.rg.name
   virtual_network_name                            = var.vnet_name
   address_prefixes                                = var.subnet_address_space
-  service_endpoints                               = element(var.service_endpoints,count.index)
+  service_endpoints                               = var.service_endpoints
   enforce_private_link_endpoint_network_policies  = var.enforce_private_link_endpoint_network_policies
 }
