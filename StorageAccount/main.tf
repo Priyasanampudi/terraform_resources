9d5509576a773a7968c439f66d536d8e14ef1b61
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 12:31:02 +0530

    Update main.tf

--- StorageAccount/main.tf
+++ StorageAccount/main.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
 }
 data "azurerm_virtual_network" "vnet" {
-    name                     = var.virtual_network_name
+    name                     = var.vnet_name
     resource_group_name      = var.resource_group_name
 }
 module "coresubnet" {
