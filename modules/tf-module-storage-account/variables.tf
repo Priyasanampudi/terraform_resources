04fc491abd2f7ad4287535e3742c86d620f98586
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 12:17:18 +0530

    Update variables.tf

--- modules/tf-module-storage-account/variables.tf
+++ modules/tf-module-storage-account/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "resource_group_name" {
     type = string
     description = "Resource group name"
@@ -22,28 +21,3 @@ variable "account_replication_type" {
 variable "tags" {
     description = "Provide tags for this resource"
 }
-=======
-variable "resource_group_name" {
-    type = string
-    description = "Resource group name"
-}
-variable "region_name" {
-    type = string
-    description = "Region name"
-}
-variable "storage_account_name" {
-    type = string
-    description = "Storage account name"
-}
-variable "account_tier" {
-    type = string
-    description = "Account tier"
-}
-variable "account_replication_type" {
-    type = string
-    description = "Account replication type"
-}
-variable "tags" {
-    description = "Provide tags for this resource"
-}
->>>>>>> dc1931cff0bb0ef136ae83715bd1e41cb4b8c026
