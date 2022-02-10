3cca0d0ec1eac29e8875d8d06cc6ac973d805ffe
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-10 17:25:13 +0530

    Update main.tf

--- modules/tf-module-storage-account/main.tf
+++ modules/tf-module-storage-account/main.tf
@@ -1,7 +1,7 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
-resource "azurerm_storage_account" "storageAccount" {
+resource "azurerm_storage_account" "stacnt" {
   name                     = var.storage_account_name
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
