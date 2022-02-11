3fc72cb242dc0780b6dbcf76876e5d69a04197ae
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-11 12:27:16 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -2,25 +2,6 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-module "nsg" {
-    source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-nsg?ref=main"
-    nsg_name                  = var.network_security_group_name
-    resource_group_name       = azurerm_resource_group.rg.name
-    tags                      = var.tags
-}
-resource "azurerm_network_security_rule" "nsgrules" {
-  name                        = var.network_security_rule_name
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = var.network_security_group_name
-}
 module "vnet" {
   source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-vnet?ref=main"
   vnet_name                 = var.vnet_name
