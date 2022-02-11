c72af7dfa57cb015143d01ca3429a950fe442a91
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-11 11:46:22 +0530

    Add files via upload

---
+++ modules/tf-module-nsg/main.tf
@@ -0,0 +1,11 @@
+# -------------- Read Resource Gorup ----------------
+data "azurerm_resource_group" "rg" {
+    name = var.resource_group_name
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = var.tags
+}
---
+++ modules/tf-module-nsg/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+    type = string
+    description = "Resource group name"
+}
+variable "nsg_name" {
+    type = string
+    description = "Name of the network security group"
+}
+variable "tags" {
+    
+}
