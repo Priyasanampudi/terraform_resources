273c771ac3e6b98be94ea001da900c9d3d008012
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-11 12:16:16 +0530

    Update main.tf

--- nsg/main.tf
+++ nsg/main.tf
@@ -1,11 +1,7 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
 module "nsg" {
     source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-nsg?ref=main"
     nsg_name                  = var.network_security_group_name
-    resource_group_name       = azurerm_resource_group.rg.name
+    resource_group_name       = var.resource_group_name
     tags                      = var.tags
 }
 resource "azurerm_network_security_rule" "nsgrules" {
@@ -18,6 +14,6 @@ resource "azurerm_network_security_rule" "nsgrules" {
   destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = var.resource_group_name
   network_security_group_name = var.network_security_group_name
 }
