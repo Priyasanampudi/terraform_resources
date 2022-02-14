db32a33b76a684e0e8593b08b42b90559ffdbfed
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-14 17:29:24 +0530

    Update main.tf

--- APIManagement/main.tf
+++ APIManagement/main.tf
@@ -13,7 +13,7 @@ data "azurerm_subnet" "apim" {
 resource "azurerm_api_management" "apim" {
   name                  = var.api_management
   location              = var.location
-  resource_group_name   = data.azurerm_resource_group.rg
+  resource_group_name   = var.resource_group_name
   publisher_name        = var.publisher_name
   publisher_email       = var.publisher_email
   sku_name              = var.sku_name 
