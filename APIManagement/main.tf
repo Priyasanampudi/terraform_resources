0939e849a96ff2c1ad587c65285ae01800555e76
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-14 17:31:56 +0530

    Update main.tf

--- APIManagement/main.tf
+++ APIManagement/main.tf
@@ -1,12 +1,9 @@
 data "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
 }
-data "azurerm_virtual_network" "vnet" {
-    name                     = var.vnet_name
-    resource_group_name      = var.resource_group_name
-}
 data "azurerm_subnet" "apim" {
     name                     = var.subnet_name
+    virtual_network_name     = var.vnet_name
     resource_group_name      = var.resource_group_name
 }
 
