ccee4c95a725eb811e632c6f67210027502f6d7c
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-17 23:19:26 +0530

    Update variables.tf

--- modules/azure-rg/variables.tf
+++ modules/azure-rg/variables.tf
@@ -1,9 +1,11 @@
 variable "location" {
   description = "Resource Group Location"
+  type = string
   default = "eastus"
 }
 variable "resource_group" {
   description = "Resource Group name"
+  type = string
 }
 variable "tags" {
   description  = "Tags to be applied"
