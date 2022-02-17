6fdd1803d9c26c7517b69b476e6bc4ce2da3c9bc
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-17 23:38:06 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,12 @@
+variable "location" {
+  description = "Name of the location where the resources will be provisioned"
+  type = string
+}
 
+variable "resource_group" {
+  description = "Name of the resource group"
+  type = string
+}
 variable "tags" {
 }
 
