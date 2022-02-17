ef3239ee7efe2147f182e55d013a8423b9bde584
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-17 22:49:39 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,56 +2,5 @@
 variable "resource_group_name"{
 
 }
-
-variable "storage_account_name"{
-
-}
-
-variable "container_name" {
-
-}
-
-variable "key" {
-
-}
-
-variable "access_key" {
-
-}
-####
 variable "location" {
 }
-
-variable "rgname" {
-}
-
-variable "Application" {
-
-}
-
-variable "BusinessUnit" {
-
-}
-
-variable "CostCenter" {
-
-}
-
-variable "RegionName" {
-
-}
-variable "Owner" {
-
-}
-
-variable "snet-name" {
-
-}
-
-# variable "k8sapikubernetes_client_id" {
-
-# }
-
-# variable "k8sapikubernetes_client_secret" {
-
-# }
