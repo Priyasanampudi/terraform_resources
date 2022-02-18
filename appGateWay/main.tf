1f78f9c1f3746651d88f8911179a18b99a297229
Priyasanampudi <37335407+Priyasanampudi@users.noreply.github.com>
2022-02-18 07:01:24 +0530

    Update main.tf

--- appGateWay/main.tf
+++ appGateWay/main.tf
@@ -1,120 +1,214 @@
-data "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "example"{
+    name = "ansumantest"
 }
 
-#--------------- Create subnet ----------------
-module "agwsubnet" {
-  source               = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-subnet?ref=main"
-  vnet_name            = var.vnet_name
-  resource_group_name  = var.resource_group_name
-  subnet_name          = var.subnet_name
-  subnet_address_space = var.subnet_address_space
-  service_endpoints    = var.service_endpoints
+resource "azurerm_user_assigned_identity" "base" {
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  name                = "mi-appgw-keyvault"
 }
 
-resource "azurerm_network_security_rule" "Allow_GWM" {
-  name                        = "Allow_GWM"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "65200-65535"
-  source_address_prefix       = "GatewayManager"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.nsg_name
-}
 
-# resource "azurerm_network_security_rule" "Allow_AzureLoadBalancer" {
-#   name                        = "Allow_AzureLoadBalancer"
-#   priority                    = 110
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "*"
-#   source_port_range           = "*"
-#   destination_port_range      = "*"
-#   source_address_prefix       = "AzureLoadBalancer"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = var.resource_group_name
-#   network_security_group_name = var.nsg_name
-#   depends_on                  = [module.nsg]
-# }
+resource "azurerm_key_vault" "kv" {
+  name                       = "ansumankeyvault01"
+  location                   = data.azurerm_resource_group.example.location
+  resource_group_name        = data.azurerm_resource_group.example.name
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  sku_name = "standard"
+  access_policy {
+    object_id    = data.azurerm_client_config.current.object_id
+    tenant_id    = data.azurerm_client_config.current.tenant_id
 
-resource "azurerm_network_security_rule" "DenyAllInbound_Internet" {
-  name                        = "DenyAllInbound_Internet"
-  priority                    = 4096
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "Internet"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.nsg_name
-}
+    certificate_permissions = [
+      "Create",
+      "Delete",
+      "DeleteIssuers",
+      "Get",
+      "GetIssuers",
+      "Import",
+      "List",
+      "ListIssuers",
+      "ManageContacts",
+      "ManageIssuers",
+      "Purge",
+      "SetIssuers",
+      "Update"
+    ]
 
-# resource "azurerm_subnet_network_security_group_association" "snet-nsg-association" {
-#   subnet_id                 = module.agwsubnet.id
-#   network_security_group_id = module.nsg.id
-#   depends_on                = [module.nsg]
-# }
+    key_permissions = [
+      "Backup",
+      "Create",
+      "Decrypt",
+      "Delete",
+      "Encrypt",
+      "Get",
+      "Import",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Sign",
+      "UnwrapKey",
+      "Update",
+      "Verify",
+      "WrapKey"
+    ]
 
-#------------ Create Public IP ---------------
-module "publicip" {
-  source = "git::https://github.com/Priyasanampudi/terraform_resources.git//modules/tf-module-public-ip?ref=main"
-  resource_group_name     = var.resource_group_name
-  public_ip_name          = var.public_ip_name
-  pip_allocation_method   = var.pip_allocation_method
-  public_ip_sku           = var.public_ip_sku
-  tags                    = var.tags
-  depends_on = [module.agwsubnet]
-}
+    secret_permissions = [
+      "Backup",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "Restore",
+      "Restore",
+      "Set"
+    ]
+  }
 
-# ---------- local block variables ------------
-locals {
-  frontend_ip_configuration_name = "${var.vnet_name}-feip"
-  frontend_ip_configuration_name_public = "${var.vnet_name}-feip-public"
-  backend_address_pool_name      = "${var.vnet_name}-beap"
-  frontend_port_name             = "${var.vnet_name}-feport"
-  http_setting_name              = "${var.vnet_name}-be-htst"
-  listener_name                  = "${var.vnet_name}-httplstn"
-  request_routing_rule_name      = "${var.vnet_name}-rqrt"
-  capacity = {
-    min = 1 #Minimum capacity for autoscaling. Accepted values are in the range 0 to 100.
-    max = 3 #Maximum capacity for autoscaling. Accepted values are in the range 2 to 125.
+  access_policy {
+    object_id    = azurerm_user_assigned_identity.base.principal_id
+    tenant_id    = data.azurerm_client_config.current.tenant_id
+
+    secret_permissions = [
+      "Get"
+    ]
   }
 }
-# ---------- Create Application Gateway ------------
+
+output "secret_identifier" {
+  value = azurerm_key_vault_certificate.example.secret_id
+}
+
+resource "azurerm_key_vault_certificate" "example" {
+  name         = "generated-cert"
+  key_vault_id = azurerm_key_vault.kv.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = ["internal.contoso.com", "domain.hello.world"]
+      }
+
+      subject            = "CN=hello-world"
+      validity_in_months = 12
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  address_space       = ["10.254.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.254.0.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.254.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku = "standard"
+}
+
+#&nbsp;since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
+
+}
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_240_seconds" {
+  depends_on = [azurerm_key_vault.kv]
+
+  create_duration = "240s"
+}
+
 resource "azurerm_application_gateway" "network" {
-  name                = var.app_gateway_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  name                = "example-appgateway"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
 
   sku {
-    name     = var.sku.name
-    tier     = var.sku.tier
-  }
-  autoscale_configuration {
-    min_capacity = local.capacity.min
-    max_capacity = local.capacity.max
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
   }
 
   gateway_ip_configuration {
-    name      = var.gateway_ip_configuration.name
-    subnet_id = module.agwsubnet.id
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
   }
 
   frontend_port {
     name = local.frontend_port_name
-    port = 80
+    port = 443
   }
 
   frontend_ip_configuration {
     name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = module.publicip.id
-    
+    public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
@@ -125,8 +219,8 @@ resource "azurerm_application_gateway" "network" {
     name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
     path                  = "/path1/"
-    port                  = 80
-    protocol              = "Http"
+    port                  = 443
+    protocol              = "Https"
     request_timeout       = 60
   }
 
@@ -134,7 +228,18 @@ resource "azurerm_application_gateway" "network" {
     name                           = local.listener_name
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
+    protocol                       = "Https"
+    ssl_certificate_name = "app_listener"
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.base.id]
+  }
+
+  ssl_certificate {
+    name = "app_listener"
+    key_vault_secret_id = azurerm_key_vault_certificate.example.secret_id
   }
 
   request_routing_rule {
@@ -144,6 +249,5 @@ resource "azurerm_application_gateway" "network" {
     backend_address_pool_name  = local.backend_address_pool_name
     backend_http_settings_name = local.http_setting_name
   }
-
-  tags                         = var.tags
+  depends_on = [time_sleep.wait_240_seconds]
 }
